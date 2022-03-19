50ba3282bd3e8d80f150d56b1689a80b0e7fd21a
Oscar Veldman <oveldman@gmail.com>
2022-03-20 00:32:28 +0100

    Test b2c

--- MadWorld/MadWorld.API/GetMessageB2C.cs
+++ MadWorld/MadWorld.API/GetMessageB2C.cs
@@ -14,7 +14,7 @@ namespace MadWorld.API
     {
         [FunctionName("GetMessageB2C")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.User, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
