1ff0524f95568e5c033e57054b6609a81c679d62
Oscar Veldman <oveldman@gmail.com>
2022-03-19 17:11:47 +0100

    Fix autorization

--- MadWorld/MadWorld.Website/Extentions/MadWorldAuthorizationMessageHandler.cs
+++ MadWorld/MadWorld.Website/Extentions/MadWorldAuthorizationMessageHandler.cs
@@ -13,7 +13,7 @@ namespace MadWorld.Website.Extentions
             ConfigureHandler(
                 authorizedUrls: new[] { "https://api.mad-world.nl" },
                 // NOTE: here with "api://"
-                scopes: new[] { "api://nlMadWorld.onmicrosoft.com/7ea82c29-9d1c-4ecb-9641-5a9e9cf84bb6/Api.ReadWrite" });
+                scopes: new[] { "https://nlMadWorld.onmicrosoft.com/7ea82c29-9d1c-4ecb-9641-5a9e9cf84bb6/Api.ReadWrite" });
         }
     }
 }
