3686f8ac4c1a49c0a0e6fda6a7fe34feea2521d3
Oscar Veldman <oveldman@gmail.com>
2022-03-19 12:08:00 +0100

    Add AzureAD

--- MadWorld/MadWorld.Website/Program.cs
+++ MadWorld/MadWorld.Website/Program.cs
@@ -8,5 +8,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMsalAuthentication(options =>
+{
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://nlMadWorld.onmicrosoft.com/7ea82c29-9d1c-4ecb-9641-5a9e9cf84bb6/Api.ReadWrite");
+});
+
 await builder.Build().RunAsync();
 
