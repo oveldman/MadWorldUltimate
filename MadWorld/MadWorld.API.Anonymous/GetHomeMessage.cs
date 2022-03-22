72bd6740d7bf50b7387a12de90d77bb049fc04a1
Oscar Veldman <oveldman@gmail.com>
2022-03-22 23:11:17 +0100

    Add extra API

--- MadWorld/MadWorld.API.Anonymous/GetHomeMessage.cs
+++ MadWorld/MadWorld.API.Anonymous/GetHomeMessage.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace MadWorld.API
+namespace MadWorld.API.Anonymous
 {
     public static class GetHomeMessage
     {
