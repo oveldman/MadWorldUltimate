e1354bbea6f284113849133ba9c9400847140c46
Oscar Veldman <oveldman@gmail.com>
2022-04-09 20:32:12 +0200

    Add Guid Generator

--- MadWorld/MadWorld.Website/Pages/Tools/GuidGenerator.razor.cs
+++ MadWorld/MadWorld.Website/Pages/Tools/GuidGenerator.razor.cs
@@ -7,13 +7,48 @@ namespace MadWorld.Website.Pages.Tools
 	{
 		private MonacoEditor _editor;
 
+		private int amountOfGuids = 0;
+
 		private async Task GenerateGuid()
         {
-			string guid = Guid.NewGuid().ToString();
-			string guid2 = Guid.NewGuid().ToString();
-			string editorValue = $"{guid}{Environment.NewLine}{guid2}";
-			await _editor.SetValue(editorValue);
+			ValidateAmountOfGuids();
+
+			string newEditorValue = string.Empty;
+			bool isFirstGuid = true;
+
+			for (int i = 0; i < amountOfGuids; i++)
+            {
+				newEditorValue = AddNewGuidToString(newEditorValue, isFirstGuid);
+				isFirstGuid = false;
+			}
+
+			await _editor.SetValue(newEditorValue);
         }
+
+		private void ValidateAmountOfGuids()
+        {
+			if (amountOfGuids < 0)
+            {
+				amountOfGuids = 0;
+            }
+
+			if (amountOfGuids > 10000)
+            {
+				amountOfGuids = 10000;
+            }
+        }
+
+		private string AddNewGuidToString(string value, bool isFirstGuid)
+        {
+			if (!isFirstGuid)
+			{
+				value += Environment.NewLine;
+			}
+
+			value += Guid.NewGuid().ToString();
+
+			return value;
+		}
 	}
 }
 
