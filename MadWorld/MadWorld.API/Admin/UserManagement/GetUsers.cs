992d85b9a0040f02cc496f84ecc744b271e3987e
Oscar Veldman <oveldman@gmail.com>
2022-04-13 21:33:33 +0200

    Test b2c roles

--- MadWorld/MadWorld.API/Admin/UserManagement/GetUsers.cs
+++ MadWorld/MadWorld.API/Admin/UserManagement/GetUsers.cs
@@ -32,7 +32,7 @@ namespace MadWorld.API.Admin.UserManagement
 
             var identity = req?.HttpContext?.User?.Identity as ClaimsIdentity;
             string email = identity?.Claims?.FirstOrDefault(c => c.Type == "emails")?.Value ?? string.Empty;
-            string responseMessage = $"You got some users and you log in as: {email}";
+            string responseMessage = $"You got some users and you log in as: {identity?.Claims?.Count()}";
 
             return new OkObjectResult(responseMessage);
         }
