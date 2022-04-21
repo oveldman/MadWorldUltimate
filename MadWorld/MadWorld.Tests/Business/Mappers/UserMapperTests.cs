d127ce7ffbdd3adb35c75560ec7b85396cde9a5e
Oscar Veldman <oveldman@gmail.com>
2022-04-21 16:19:55 +0200

    Add project unittests

---
+++ MadWorld/MadWorld.Tests/Business/Mappers/UserMapperTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace MadWorld.Tests.Business.Mappers
+{
+	public class UserMapperTests
+	{
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+
