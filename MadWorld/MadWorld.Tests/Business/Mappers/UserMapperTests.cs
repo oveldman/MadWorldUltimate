56eb1c6289c6e8bb9d040683879f428c0b3c094f
Oscar Veldman <oveldman@gmail.com>
2022-04-21 17:13:00 +0200

    Add first unittest

--- MadWorld/MadWorld.Tests/Business/Mappers/UserMapperTests.cs
+++ MadWorld/MadWorld.Tests/Business/Mappers/UserMapperTests.cs
@@ -1,12 +1,35 @@
-﻿namespace MadWorld.Tests.Business.Mappers
+﻿using MadWorld.Business.Mappers;
+using MadWorld.Data.TableStorage.Tables;
+using MadWorld.Shared.Models.API.Users;
+
+namespace MadWorld.Tests.Business.Mappers
 {
 	public class UserMapperTests
 	{
-        [Fact]
-        public void Test1()
-        {
+		[Theory]
+		[AutoDomainData]
+		public void Translate_User_UserModel(
+			UserMapper userMapper,
+			User user)
+			{
+				// No Test data
 
-        }
-    }
+				// No Setup
+
+				// Act
+				UserModel userModel = userMapper.Translate<User, UserModel>(user);
+
+				// Assert
+				UserModel expectedModel = new()
+				{
+					ID = user.RowKey,
+					Email = user.Email
+				};
+
+				expectedModel.Should().BeEquivalentTo(userModel);
+
+				// No Teardown
+			}
+	}
 }
 
--- MadWorld/MadWorld.Tests/GlobalUsings.cs
+++ MadWorld/MadWorld.Tests/GlobalUsings.cs
@@ -1,5 +1,8 @@
 ﻿global using AutoFixture;
 global using AutoFixture.AutoMoq;
 global using AutoFixture.Xunit2;
+global using FluentAssertions;
 global using Xunit;
+
+global using MadWorld.Tests.General;
 
