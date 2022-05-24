2de016a0fa734eab7102b46a707cb7a2c1719811
Oscar Veldman <oveldman@gmail.com>
2022-05-25 00:00:46 +0200

    Add version number

--- MadWorld/MadWorld.Website/wwwroot/service-worker.published.js
+++ MadWorld/MadWorld.Website/wwwroot/service-worker.published.js
@@ -1,4 +1,6 @@
-﻿// Caution! Be sure you understand the caveats before publishing an application with
+﻿const CACHE_VERSION = '{#CACHE_VERSION#}'
+
+// Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
 self.importScripts('./service-worker-assets.js');
