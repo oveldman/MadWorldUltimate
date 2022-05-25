ee978bea0666dff3d8ed3035d29f3b448d9b6654
Oscar Veldman <oveldman@gmail.com>
2022-05-25 19:12:34 +0200

    Edit Cache name

--- MadWorld/MadWorld.Website/wwwroot/service-worker.published.js
+++ MadWorld/MadWorld.Website/wwwroot/service-worker.published.js
@@ -9,7 +9,7 @@ self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
-const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}${CACHE_VERSION}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
 const offlineAssetsExclude = [/^service-worker\.js$/, /^security\.txt$/ ];
 
