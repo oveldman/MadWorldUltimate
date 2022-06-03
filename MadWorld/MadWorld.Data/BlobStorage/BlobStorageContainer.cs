394e9c03201f8198a9c5316720882d2cfd531f8a
Oscar Veldman <oveldman@gmail.com>
2022-06-04 00:21:53 +0200

    Finish download and upload blobs

--- MadWorld/MadWorld.Data/BlobStorage/BlobStorageContainer.cs
+++ MadWorld/MadWorld.Data/BlobStorage/BlobStorageContainer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Azure.Storage.Blobs;
 using MadWorld.Data.BlobStorage.Interfaces;
 
@@ -12,6 +13,56 @@ namespace MadWorld.Data.BlobStorage
         {
 			_containerClient = blobServiceClient.CreateBlobContainer(name);
         }
+
+		public Stream Download(string fileName)
+        {
+			BlobClient client = _containerClient.GetBlobClient(fileName);
+
+			var result = client.DownloadContent();
+			var response = result.GetRawResponse();
+
+			if (response.IsError)
+            {
+				return new MemoryStream();
+            }
+
+			return response.ContentStream ?? new MemoryStream();
+		}
+
+		public byte[] DownloadBytes(string fileName)
+		{
+			Stream body = Download(fileName);
+
+            using var memoryStream = new MemoryStream();
+            body.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
+
+		public string DownloadString(string fileName)
+        {
+			byte[] body = DownloadBytes(fileName);
+			return Encoding.Default.GetString(body);
+		}
+
+		public bool Upload(string fileName, Stream body)
+        {
+			BlobClient client = _containerClient.GetBlobClient(fileName);
+
+			var result = client.Upload(body);
+			var response = result.GetRawResponse();
+			return !response.IsError;
+		}
+
+		public bool Upload(string fileName, byte[] body)
+        {
+			return Upload(fileName, new MemoryStream(body));
+        }
+
+		public bool Upload(string fileName, string body)
+		{
+			byte[] bodyBytes = Encoding.ASCII.GetBytes(body);
+			return Upload(fileName, bodyBytes);
+		}
 	}
 }
 
