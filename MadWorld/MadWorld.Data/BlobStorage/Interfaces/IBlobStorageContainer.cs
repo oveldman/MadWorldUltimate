aa34228dd9cd82a76ecb8f172feff10e91601645
Oscar Veldman <oveldman@gmail.com>
2022-06-04 00:23:17 +0200

    Add to interface

--- MadWorld/MadWorld.Data/BlobStorage/Interfaces/IBlobStorageContainer.cs
+++ MadWorld/MadWorld.Data/BlobStorage/Interfaces/IBlobStorageContainer.cs
@@ -3,6 +3,12 @@ namespace MadWorld.Data.BlobStorage.Interfaces
 {
 	public interface IBlobStorageContainer
 	{
+		Stream Download(string fileName);
+		byte[] DownloadBytes(string fileName);
+		string DownloadString(string fileName);
+		bool Upload(string fileName, Stream body);
+		bool Upload(string fileName, byte[] body);
+		bool Upload(string fileName, string body);
 	}
 }
 
