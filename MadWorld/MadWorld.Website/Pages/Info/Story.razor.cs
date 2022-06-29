08c7b54df38f01fdfd60f5e7cd7f8dd286509342
Oscar Veldman <oveldman@gmail.com>
2022-06-29 21:22:45 +0200

    add Markup string

---
+++ MadWorld/MadWorld.Website/Pages/Info/Story.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace MadWorld.Website.Pages.Info
+{
+	public partial class Story
+	{
+        private readonly MarkupString body = new("<h1>Story</h1>");
+    }
+}
+
