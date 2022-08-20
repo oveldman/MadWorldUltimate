ddfdcde53b79c781560021cdb516f0b797ec06f3
Oscar Veldman <oveldman@gmail.com>
2022-08-20 17:38:46 +0200

    refactor exception

--- MadWorld/MadWorld.Shared/Exceptions/ExceptionNotFoundException.cs
+++ MadWorld/MadWorld.Shared/Exceptions/ExceptionNotFoundException.cs
@@ -1,7 +1,15 @@
+using System.Runtime.Serialization;
+
 namespace MadWorld.Shared.Exceptions;
 
 [Serializable]
 public class ExceptionNotFoundException : Exception
 {
+    public ExceptionNotFoundException()
+    {
+    }
     
+    protected ExceptionNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
 }
