83ac8b354463926c0522de820d45241d825d8c26
Oscar Veldman <oveldman@gmail.com>
2022-09-05 22:08:56 +0200

    Fix small bug

--- MadWorld/MadWorld.API.Anonymous/Info/GetStory.cs
+++ MadWorld/MadWorld.API.Anonymous/Info/GetStory.cs
@@ -2,7 +2,6 @@
 using MadWorld.Functions.Common.Info;
 using MadWorld.Shared.Models.AnonymousAPI.Info;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -12,18 +11,14 @@ namespace MadWorld.API.Anonymous.Info
 	public class GetStory
 	{
         [FunctionName(nameof(GetStory))]
-        public IActionResult Run(
+        public ResponseStory Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, RequestType.Get, Route = null)] HttpRequest req,
             ILogger log)
         {
-            return new StatusCodeResult(StatusCodes.Status503ServiceUnavailable);
-            
-            /*
             return new()
             {
                 Body = "<h1>Story</h1>"
             };
-            */
         }
     }
 }
