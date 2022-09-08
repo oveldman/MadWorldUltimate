bd09be75d90056d8186747a6d90ecb82aeb21bd6
Oscar Veldman <oveldman@gmail.com>
2022-09-08 14:11:15 +0200

    Add more unittests

--- MadWorld/MadWorld.Tests/Api.Anonymous/Tests/GetReboundTests.cs
+++ MadWorld/MadWorld.Tests/Api.Anonymous/Tests/GetReboundTests.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text;
 using MadWorld.API.Anonymous.Tests;
 using MadWorld.Functions.Common.Info;
 using Microsoft.AspNetCore.Http;
@@ -33,6 +34,29 @@ public class GetReboundTests
 
         // Assert
         Assert.Equal(queryItem, result);
+        
+        // No Teardown
+    }
+    
+    [Theory]
+    [AutoDomainData]
+    public async Task Run_RequestWithBody_ReboundRequest(
+        string bodyText,
+        Mock<HttpRequest> request,
+        Mock<ILogger> logger)
+    {
+        // Test data
+        var query = new QueryCollection();
+        
+        // Setup
+        request.Setup(r => r.Body).Returns(new MemoryStream(Encoding.UTF8.GetBytes(bodyText)));
+        request.Setup(r => r.Query).Returns(query);
+        
+        // Act
+        var result = await GetRebound.Run(request.Object, logger.Object);
+
+        // Assert
+        Assert.Equal(bodyText, result);
         
         // No Teardown
     }
--- MadWorld/MadWorld.Tests/Api.Anonymous/Tests/GetStatusCodeTests.cs
+++ MadWorld/MadWorld.Tests/Api.Anonymous/Tests/GetStatusCodeTests.cs
@@ -1,6 +1,8 @@
 using System.IO;
 using MadWorld.API.Anonymous.Tests;
 using MadWorld.Functions.Common.Info;
+using MadWorld.Shared.Common;
+using MadWorld.Shared.Models.AnonymousAPI.Tests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc;
@@ -15,7 +17,7 @@ public class GetStatusCodeTests
     [AutoDomainInlineData("200", 200)]
     [AutoDomainInlineData("403", 403)]
     [AutoDomainInlineData("503", 503)]
-    public async Task Run_StatusCode_ThrowError(
+    public async Task Run_StatusCodeQuery_ThrowError(
         string statusCode, 
         int expectedStatusCode,
         GetStatusCode getStatusCode,
@@ -31,6 +33,37 @@ public class GetStatusCodeTests
 
         // Setup
         request.Setup(r => r.Body).Returns(new MemoryStream());
+        request.Setup(r => r.Query).Returns(query);
+
+        // Act
+        var result = await getStatusCode.Run(request.Object, logger.Object);
+        var statusCodeResult = result as StatusCodeResult;
+
+        // Assert
+        Assert.NotNull(statusCodeResult);
+        Assert.Equal(expectedStatusCode, statusCodeResult.StatusCode);
+
+        //No Teardown
+    }
+    
+    [Theory]
+    [AutoDomainInlineData("200", 200)]
+    [AutoDomainInlineData("403", 403)]
+    [AutoDomainInlineData("503", 503)]
+    public async Task Run_StatusCodeBody_ThrowError(
+        string statusCode, 
+        int expectedStatusCode,
+        StatusCodeDto statusCodeRequest,
+        GetStatusCode getStatusCode,
+        Mock<ILogger> logger,
+        Mock<HttpRequest> request)
+    {
+        // Test data
+        var query = new QueryCollection();
+        statusCodeRequest.StatusCode = statusCode;
+
+        // Setup
+        request.Setup(r => r.Body).Returns(StreamConverter.Convert(statusCodeRequest));
         request.Setup(r => r.Query).Returns(query);
 
         // Act
