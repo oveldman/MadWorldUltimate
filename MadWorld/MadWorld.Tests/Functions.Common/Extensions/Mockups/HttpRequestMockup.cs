43eb9a84626654bdbe76c82e499820765780c4c9
Oscar Veldman <oveldman@gmail.com>
2022-09-19 11:16:25 +0200

    Refactor unittests to remove TODO

--- MadWorld/MadWorld.Tests/Functions.Common/Extensions/Mockups/HttpRequestMockup.cs
+++ MadWorld/MadWorld.Tests/Functions.Common/Extensions/Mockups/HttpRequestMockup.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using MadWorld.Tests.General.Exceptions;
 using Microsoft.AspNetCore.Http;
 
 namespace MadWorld.Tests.Functions.Common.Extensions.Mockups
@@ -13,32 +14,32 @@ namespace MadWorld.Tests.Functions.Common.Extensions.Mockups
             Body = body;
 		}
 
-        public override HttpContext HttpContext => throw new NotImplementedException();
+        public override HttpContext HttpContext => throw new TestFailedException();
 
-        public override string Method { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Scheme { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override bool IsHttps { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override HostString Host { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override PathString PathBase { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override PathString Path { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override QueryString QueryString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override IQueryCollection Query { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Protocol { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string Method { get => throw new TestFailedException(); set => throw new TestFailedException(); }
+        public override string Scheme { get => throw new TestFailedException(); set => throw new TestFailedException(); }
+        public override bool IsHttps { get => throw new TestFailedException(); set => throw new TestFailedException(); }
+        public override HostString Host { get => throw new TestFailedException(); set => throw new TestFailedException(); }
+        public override PathString PathBase { get => throw new TestFailedException(); set => throw new TestFailedException(); }
+        public override PathString Path { get => throw new TestFailedException(); set => throw new TestFailedException(); }
+        public override QueryString QueryString { get => throw new TestFailedException(); set => throw new TestFailedException(); }
+        public override IQueryCollection Query { get => throw new TestFailedException(); set => throw new TestFailedException(); }
+        public override string Protocol { get => throw new TestFailedException(); set => throw new TestFailedException(); }
 
-        public override IHeaderDictionary Headers => throw new NotImplementedException();
+        public override IHeaderDictionary Headers => throw new TestFailedException();
 
-        public override IRequestCookieCollection Cookies { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override long? ContentLength { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string ContentType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override IRequestCookieCollection Cookies { get => throw new TestFailedException(); set => throw new TestFailedException(); }
+        public override long? ContentLength { get => throw new TestFailedException(); set => throw new TestFailedException(); }
+        public override string ContentType { get => throw new TestFailedException(); set => throw new TestFailedException(); }
         public sealed override Stream Body { get; set; }
 
-        public override bool HasFormContentType => throw new NotImplementedException();
+        public override bool HasFormContentType => throw new TestFailedException();
 
-        public override IFormCollection Form { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override IFormCollection Form { get => throw new TestFailedException(); set => throw new TestFailedException(); }
 
         public override Task<IFormCollection> ReadFormAsync(CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            throw new TestFailedException();
         }
     }
 }
