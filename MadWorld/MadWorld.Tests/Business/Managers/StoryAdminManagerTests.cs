23f731258e836338a5cbc8ff58b00ea28830a9e9
Oscar Veldman <oveldman@gmail.com>
2022-09-24 14:40:40 +0200

    Add unit test to get story from API

---
+++ MadWorld/MadWorld.Tests/Business/Managers/StoryAdminManagerTests.cs
@@ -0,0 +1,144 @@
+using MadWorld.Business.Managers;
+using MadWorld.Business.Mappers.Interfaces;
+using MadWorld.Data.BlobStorage.Interfaces;
+using MadWorld.Data.TableStorage.Queries.Interfaces;
+using MadWorld.Data.TableStorage.Tables;
+using MadWorld.Shared.Models.API.Stories;
+using Shouldly;
+
+namespace MadWorld.Tests.Business.Managers;
+
+public class StoryAdminManagerTests
+{
+    [Theory]
+    [AutoDomainData]
+    public void GetConcept_NotFound_EmptyResponse(
+        [Frozen] Mock<IStoryQueries> storyQueries,
+        StoryAdminManager manager
+        )
+    {
+        // No Test data
+
+        // Setup
+        storyQueries.Setup(s => s.GetConcept()).Returns(Option.None<Story>());
+
+        // Act
+        var result = manager.GetConcept();
+
+        // Assert
+        result.IsConcept.ShouldBe(true);
+        result.Found.ShouldBe(false);
+        result.BodyBase64.ShouldBe(string.Empty);
+        
+        // No Teardown
+    }
+    
+    [Theory]
+    [AutoDomainData]
+    public void GetConcept_FoundStory_NormalResponse(
+        [Frozen] Mock<IBlobStorageContainer> container,
+        [Frozen] Mock<IStoryMapper> mapper,
+        [Frozen] Mock<IStoryQueries> storyQueries,
+        StoryAdminManager manager,
+        string base64Test,
+        Story story
+    )
+    {
+        // Test data
+        story.IsConcept = true;
+        var mockResponse = new ResponseStory()
+        {
+            Id = story.RowKey,
+            Comment = story.Comment,
+            IsConcept = story.IsConcept,
+            BodyBase64 = string.Empty
+        };
+
+        // Setup
+        container.Setup(c => 
+            c.DownloadBase64(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(base64Test);
+        mapper.Setup(m => 
+                m.Translate<Story, ResponseStory>(It.IsAny<Story>()))
+            .Returns(mockResponse);
+        storyQueries.Setup(s => s.GetConcept()).Returns(story.Some());
+
+        // Act
+        var result = manager.GetConcept();
+
+        // Assert
+        result.Id.ShouldBe(story.RowKey);
+        result.Comment.ShouldBe(story.Comment);
+        result.IsConcept.ShouldBe(true);
+        result.Found.ShouldBe(true);
+        result.BodyBase64.ShouldBe(base64Test);
+        
+        // No Teardown
+    }
+    
+    [Theory]
+    [AutoDomainData]
+    public void GetFinal_NotFound_EmptyResponse(
+        [Frozen] Mock<IStoryQueries> storyQueries,
+        StoryAdminManager manager
+    )
+    {
+        // No Test data
+
+        // Setup
+        storyQueries.Setup(s => s.GetFinal()).Returns(Option.None<Story>());
+
+        // Act
+        var result = manager.GetFinal();
+
+        // Assert
+        result.IsConcept.ShouldBe(false);
+        result.Found.ShouldBe(false);
+        result.BodyBase64.ShouldBe(string.Empty);
+        
+        // No Teardown
+    }
+    
+    [Theory]
+    [AutoDomainData]
+    public void GetFinal_FoundStory_NormalResponse(
+        [Frozen] Mock<IBlobStorageContainer> container,
+        [Frozen] Mock<IStoryMapper> mapper,
+        [Frozen] Mock<IStoryQueries> storyQueries,
+        StoryAdminManager manager,
+        string base64Test,
+        Story story
+    )
+    {
+        // Test data
+        story.IsConcept = false;
+        var mockResponse = new ResponseStory()
+        {
+            Id = story.RowKey,
+            Comment = story.Comment,
+            IsConcept = story.IsConcept,
+            BodyBase64 = string.Empty
+        };
+
+        // Setup
+        container.Setup(c => 
+                c.DownloadBase64(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(base64Test);
+        mapper.Setup(m => 
+                m.Translate<Story, ResponseStory>(It.IsAny<Story>()))
+            .Returns(mockResponse);
+        storyQueries.Setup(s => s.GetConcept()).Returns(story.Some());
+
+        // Act
+        var result = manager.GetConcept();
+
+        // Assert
+        result.Id.ShouldBe(story.RowKey);
+        result.Comment.ShouldBe(story.Comment);
+        result.IsConcept.ShouldBe(false);
+        result.Found.ShouldBe(true);
+        result.BodyBase64.ShouldBe(base64Test);
+        
+        // No Teardown
+    }
+}
